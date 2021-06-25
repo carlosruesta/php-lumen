fe3351e354fbf23955779af5f14768f1a1c2c583
Carlos Ruesta <carlos@dcide.com.br>
2021-06-25 14:12:28 -0300

    Implementando o salvar Serie

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Serie;
+use Illuminate\Http\Request;
 
 class SeriesController
 {
     public function index()
     {
         return Serie::all();
+    }
+
+    public function store(Request $request)
+    {
+        // ler o que o usuario esta mandando
+        $nome = $request->nome;
+
+        // gravar em banco
+        $retorno = Serie::create(['nome' => $nome]);
+
+        // retornar uma resposta
+        return response()->json($retorno, 201);
     }
 }
