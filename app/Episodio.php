0933fd710a65edbb65a66e209d8c7cffc8d37918
Carlos Ruesta <carlos@dcide.com.br>
2021-06-30 14:42:40 -0300

    Alterando os tipos de dados retornados pelo model

--- app/Episodio.php
+++ app/Episodio.php
@@ -9,8 +9,29 @@ class Episodio extends Model
     public $timestamps = false;
     protected $fillable = ['temporada', 'numero', 'assistido', 'serie_id'];
 
-    public function serie() {
+    public function serie()
+    {
         return $this->belongsTo(Serie::class);
+    }
+
+    public function getAssistidoAttribute($valor) : bool
+    {
+        return $valor;
+    }
+
+    public function getTemporadaAttribute($valor) : int
+    {
+        return $valor;
+    }
+
+    public function getNumeroAttribute($valor)
+    {
+        return (int) $valor;
+    }
+
+    public function getSerieIdAttribute(int $valor)
+    {
+        return $valor;
     }
 
 }
