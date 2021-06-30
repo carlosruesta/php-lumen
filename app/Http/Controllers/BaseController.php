7a9f27894a0431007766b81baa05972649669b30
Carlos Ruesta <carlos@dcide.com.br>
2021-06-30 14:11:44 -0300

    Implementando o controller dos Episodios, refactoring para ter um BaseController que evita a duplicacao de codigo

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+abstract class BaseController
+{
+    protected $model;
+
+    public function index()
+    {
+        return $this->model::all();
+    }
+
+    public function store(Request $request)
+    {
+        // ler o que o usuario esta mandando
+        $dados = $request->all();
+
+        // gravar em banco
+        $retorno = $this->model::create($dados);
+
+        // retornar uma resposta
+        return response()->json($retorno, 201);
+    }
+
+    public function show(int $id)
+    {
+        // solicitar ao banco
+        $serie = $this->model::find($id);
+
+        if (is_null($serie)) {
+            return response()->json(null, 204);
+        }
+
+        // conversao de dados
+        return response()->json($serie);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        // solicitar ao banco
+        $serie = $this->model::find($id);
+
+        if (is_null($serie)) {
+            return response()->json([
+                'sucesso' => false,
+                'mensagem' => 'Série não encontrada'
+            ], 404);
+        }
+
+        // gravar em banco
+        $serie->fill($request->all());
+
+        $serie->save();
+
+        // conversao de dados
+        return response()->json($serie);
+    }
+
+    public function destroy(int $id)
+    {
+        $qtdRecursosRemovidos = $this->model::destroy($id);
+
+        if ($qtdRecursosRemovidos === 0) {
+            return response()->json([
+                "erro" => "Recurso não encontrado"
+            ], "404");
+        }
+
+        return response()->json([], 204);
+    }
+
+}
