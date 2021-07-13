c838d2b81a077ecbbd2e2b45d19db8cb3cee3b19
Carlos Ruesta <carlos@dcide.com.br>
2021-07-13 16:10:09 -0300

    Criacão e uso do Modelo de Usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,13 +13,14 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
 {
     use Authenticatable, Authorizable, HasFactory;
 
+    protected $table = 'usuarios';
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
+        'email'
     ];
 
     /**
@@ -28,6 +29,6 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $hidden = [
-        'password',
+        'senha',
     ];
 }
